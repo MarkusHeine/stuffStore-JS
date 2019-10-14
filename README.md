1b16d31da018da7acd8566f6f3fb90952a11a7a5
MarkusHeine <markusheine.dev@gmail.com>
2019-10-14 15:40:20 +0200

    remove console logs

--- README.md
+++ README.md
@@ -1,5 +1,7 @@
-`simpleSwipeDetection.js` is a program for a simple swipe detection on mobile devices
-
 # stuffStore-JS
 
 usefull JavaScript Code
+
+####simpleSwipeDetection.js
+
+is a program for a simple swipe detection on mobile devices
--- simpleSwipeDetection.js
+++ simpleSwipeDetection.js
@@ -6,28 +6,19 @@ var swipeDirection = "";
 var swipe = "false";
 
 document.addEventListener("touchstart", function(e) {
-    //log x & y start points
-    // console.log("x Start: " + e.changedTouches[0].screenX);
-    // console.log("y Start: " + e.changedTouches[0].screenY);
     touchstartX = e.changedTouches[0].screenX;
     touchstartY = e.changedTouches[0].screenY;
 });
 
 document.addEventListener("touchend", function(e) {
-    // log X & X end points
-    // console.log("x End: " + e.changedTouches[0].screenX);
-    // console.log("y End: " + e.changedTouches[0].screenY);
     touchendX = e.changedTouches[0].screenX;
     touchendY = e.changedTouches[0].screenY;
     checkSwipe();
 });
 
-// check if the sipmple direction of the swipe and if user has swiped
-
 function checkSwipe() {
     var x = touchstartX - touchendX;
     var y = touchstartY - touchendY;
-    console.log(x, y);
 
     if (x > 100) {
         swipe = true;
@@ -44,5 +35,4 @@ function checkSwipe() {
     } else {
         swipe = false;
     }
-    // console.log(swipe, swipeDirection);
 }
